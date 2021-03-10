609990912cc76c8fd0a0544edc77a5bbfefb4288
twenty5151 <tsuiyikching@protonmail.com>
2021-03-10 14:14:04 +0000

    Replace one instance of `any`

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,7 @@
 import { browser } from "webextension-polyfill-ts"
 
 browser.runtime.onMessage.addListener(
-    (message: any) => browser.tabs.create({url: message.url, active: false})
+    (message: {url: string}) => browser.tabs.create({
+        url: message.url, active: false
+    })
 )
