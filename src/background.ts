78aac983af8bab349307b55c2792c655da3e85a0
twenty5151 <tsuiyikching@protonmail.com>
2021-05-25 17:30:20 +0100

    Fix bug where newly opened tab is not immediately next to current tab

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,12 @@
 import { browser } from "webextension-polyfill-ts"
 
-browser.runtime.onMessage.addListener(
-    (message: {url: string}) => browser.tabs.create({
-        url: message.url, active: false
+async function createTab(message: {url: string}) {
+    const index: number = await browser.tabs.query(
+        {currentWindow: true, active: true}
+    ).then(tabs => tabs[0].index)
+    browser.tabs.create({
+        url: message.url, active: false, index: index + 1
     })
-)
+}
+
+browser.runtime.onMessage.addListener(createTab)
