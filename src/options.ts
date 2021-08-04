f9bccde20603a73c97ff37808e998915934ad767
twenty5151 <tsuiyikching@protonmail.com>
2021-08-04 14:37:23 +0100

    Remove `any` in options.ts and fix bug where settings aren't set

--- src/options.ts
+++ src/options.ts
@@ -1,10 +1,10 @@
 import { browser } from "webextension-polyfill-ts"
 
-function unwrapOr(nullable: any, default_: string) {
+function unwrapOr(nullable: HTMLInputElement | null, default_: string): string {
     return nullable ? nullable.value : default_
 }
 
-function setOrDefault<V>(map: Map<string, V>, name: string, default_: string) {
+function setOrDefault(map: Map<string, string>, name: string, default_: string) {
     map.set(name, unwrapOr(document.querySelector('#' + name), default_))
 }
 
@@ -21,24 +21,17 @@ function saveOptions(event: Event) {
     setOrDefault(map, 'incimgsize', '=')
     setOrDefault(map, 'decimgsize', '-')
     setOrDefault(map, 'incdecsize', '30')
-    browser.storage.sync.set(map)
+    browser.storage.sync.set(Object.fromEntries(map))
 }
 
-function getKey(result: any, setting: string) {
-    if (typeof result === 'string') {
-        return result
-    }
-    return result[setting] || ''
-}
-
-function onSuccess(result: any, setting: string) {
-    let field: any = document.querySelector(`#${setting}`)
+function onSuccess(result: { [i: string]: string }, setting: string) {
+    let field: HTMLInputElement | null = document.querySelector(`#${setting}`)
     if (field) {
-        field.value = getKey(result, setting)
+        field.value = result[setting]
     }
 }
 
-function onError(error: any) {
+function onError(error: Error) {
     console.log(`Error: ${error}`)
 }
 
@@ -50,14 +43,13 @@ function restoreOptions() {
     ]
     for (const setting of settings) {
         browser.storage.sync.get(setting).then(
-            (r: any) => onSuccess(r, setting),
+            // object indexed with string, giving a string
+            // like Map<string, string>
+            (r: { [i: string]: string }) => onSuccess(r, setting),
             onError
         )
     }
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
-let form = document.querySelector("form")
-if (form) {
-    form.addEventListener("submit", saveOptions);
-}
+document.addEventListener("DOMContentLoaded", restoreOptions)
+document.querySelector("form")?.addEventListener("submit", saveOptions)
